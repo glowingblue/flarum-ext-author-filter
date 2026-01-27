782137b0941c242c2dbea8e51fb370ce8a488fbe
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2026-01-27 11:43:20 +0100

    chore: reimplement forum frontend

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,5 +1,11 @@
 import app from 'flarum/forum/app';
 
+import addUserFilterDropdownToIndexPage from './extenders/addUserFilterDropdownToIndexPage';
+import extendDiscussionListState from './extenders/extendDiscussionListState';
+import extendGlobalSearchState from './extenders/extendGlobalSearchState';
+
 app.initializers.add('glowingblue-author-filter', () => {
-	// nothing here yet
+	addUserFilterDropdownToIndexPage();
+	extendDiscussionListState();
+	extendGlobalSearchState();
 });
